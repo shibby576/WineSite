f122af2d4a369d3443206f2e82e767b4bb5dea70
Jonathan Hirko <jhirko12@gmail.com>
2023-01-28 18:02:00 -0500

    it works!

--- project/app.py
+++ project/app.py
@@ -12,12 +12,12 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     """Grabs the input values and uses them to make prediction"""
-    sulfate = int(request.form["sulfate"])
-    alcohol = int(request.form["alcohol"])
+    sulfate = float(request.form["sulfate"])
+    alcohol = float(request.form["alcohol"])
     prediction = model.predict([[sulfate, alcohol]])  # this returns a list e.g. [127.20488798], so pick first element [0]
     output = round(prediction[0], 2) 
 
-    return render_template('index.html', prediction_text='A house with {sulfate} rooms and located {alcohol} meters from the city center has a value of ${output}')
+    return render_template('index.html', prediction_text=f'A wine with a sulfate level of {sulfate} and alcohol level of {alcohol} % has a predicted quality of {output}')
 
 if __name__ == "__main__":
     app.run()
