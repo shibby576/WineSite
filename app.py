6454d247021de1f1f0d4408f1aabb53198b5fd78
Jonathan Hirko <jhirko12@gmail.com>
2023-02-01 09:39:12 -0500

    restructuring

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    """Grabs the input values and uses them to make prediction"""
+    sulfate = float(request.form["sulfate"])
+    alcohol = float(request.form["alcohol"])
+    prediction = model.predict([[sulfate, alcohol]])  # this returns a list e.g. [127.20488798], so pick first element [0]
+    output = round(prediction[0], 2) 
+    modified_output=''
+    output_measure=0.0
+    if output <.4 :
+        output_measure = round(3+(output*5.013),0)
+        modified_output = 'low'
+    elif output < .6:
+        output_measure = round(3+(output*5.025),0)
+        modified_output='moderate'
+    elif output < .8:
+        output_measure = round(3+(output*5.013),0)
+        modified_output='high'
+    elif output < 1:
+        output_measure = round(3+(output*5.013),0)
+        modified_output='great'
+    else:
+        output_measure = round(3+(output*5.013),0)
+        modified_output='superb'
+
+
+    return render_template('index.html', prediction_text=f'Your wine has a predicted quality of {output_measure} and a rating of {modified_output}!')
+
+if __name__ == "__main__":
+    app.run()
